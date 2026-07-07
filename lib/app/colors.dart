7cb259aa743053770e9a2d5abe7a0ce5c337f0c7
mariopruit5 <127955116+mariopruit5@users.noreply.github.com>
2026-07-07 21:36:42 +0600

    Add AppColors class for color constants

---
+++ lib/app/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF0D1117);
+  static const Color card = Color(0xFF161B22);
+
+  static const Color buy = Color(0xFF00C853);
+  static const Color sell = Color(0xFFFF1744);
+  static const Color wait = Color(0xFFFFD600);
+
+  static const Color text = Colors.white;
+}
